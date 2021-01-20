678439d6d8935583c6b1e877701ef1b8b4c1715a
vaibhav <61009100+vs-dev-ui@users.noreply.github.com>
2021-01-21 04:21:13 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,37 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+var serverUrl = "	https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(input) {
+    return serverUrl + "?" + "text=" + input;
+
+}
+
+//Handle Error
+function ErrorHandler(error) {
+    console.log('Error with the server');
+    alert('Check the server connection!');
+}
+
+function clickHandler() {
+    //1. read the input text
+    var inputText = txtInput.value;
+    //2. Go and fetch the URL
+    fetch(getTranslationURL(inputText)).
+    //3. convert url into response.json
+    then(response => response.json()).
+    //4. output it 
+    // then(json => console.log(json.contents.translated))
+    then(json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+    })
+        //5.Handle Error
+        // .catch(ErrorHandler);
+}
+
+
+
+btnTranslate.addEventListener("click", clickHandler);
+txtInput.addEventListener("click", clickHandler);
